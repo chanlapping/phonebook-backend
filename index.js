d13a94612abc7297efe12cc54a9edeb899353c64
chanlapping <chanlapping64@gmail.com>
2026-04-23 20:39:03 +0800

    connect to Mongo DB

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const Person = require("./models/person");
+
 const app = express();
 
 app.use(express.json());
@@ -14,31 +17,10 @@ app.use(
 
 app.use(express.static("dist"));
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
 app.get("/api/info", (req, res) => {
@@ -47,20 +29,28 @@ app.get("/api/info", (req, res) => {
   );
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = req.params.id;
-  const person = persons.find((p) => p.id === id);
-  if (!person) {
-    return res.status(404).end();
-  }
-  res.json(person);
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (!person) {
+        return res.status(404).end();
+      }
+
+      res.json(person);
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = req.params.id;
-  const personToDelete = persons.find((person) => person.id === id);
-  persons = persons.filter((person) => person.id !== id);
-  res.json(personToDelete);
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then((deletedPerson) => {
+      res.json(deletedPerson);
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 app.post("/api/persons", (req, res) => {
@@ -72,15 +62,48 @@ app.post("/api/persons", (req, res) => {
   if (duplicatedName) {
     return res.status(400).json({ error: "duplicated name" });
   }
-  const id = Math.ceil(Math.random() * 10000);
-  const person = {
-    id: String(id),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
-  persons = persons.concat(person);
-  res.json(person);
+  });
+  person.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
 });
+
+app.put("/api/persons/:id", (req, res, next) => {
+  const { name, number } = req.body;
+
+  Person.findById(req.params.id).then((person) => {
+    if (!person) {
+      return res.status(404).end();
+    }
+
+    person.name = name;
+    person.number = number;
+
+    person
+      .save()
+      .then((updatedPerson) => {
+        return res.json(updatedPerson);
+      })
+      .catch((error) => {
+        next(error);
+      });
+  });
+});
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return res.status(400).json({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
